b7f3f597cd1e96e5360ef07f2b1b8f105465c2f9
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-02-14 00:45:54 +0600

    string clear

--- string.js
+++ string.js
@@ -4,45 +4,49 @@ const string2 = 'AMAR SONAR BANGLA AMI TOMAI VALOBASHI '
 // console.log(string);
 // console.log(string2);
 if (string.toUpperCase() == string2.toUpperCase()) {
-    // console.log(' This Two line is Correct ');
+    console.log(' This Two line is Correct ');
 } else {
-    // console.log('nah line gulo thik nai ');
+    console.log('nah line gulo thik nai ');
 }
 
 
-
-
 // split 
-// const result = string.split();
+const result = string.split();
 /* 
 Split a string into substrings using the specified separator and return them as an array.
 @param separator — A string that identifies character or characters to use in separating the string. If omitted, a single-element array containing the entire string is returned.
 @param limit — A value used to limit the number of elements returned in the array. */
-// const split = string.split();
-// const split = string.split('amr');
-// const splitss = string.split('a');
+const split = string.split();
+const split = string.split('amr');
+const splitss = string.split('a');
 
 
 // slice 
-// const split = string.slice(' ');
+const split = string.slice(' ');
 // Returns a section of a string.
-// const slice = string.slice(' a ');
-// const slice = string.slice(4, 11);
+const slice = string.slice(' a ');
+const slice = string.slice(4, 11);
 
 // substr 
 // Gets a substring beginning at the specified location and having the specified length.
-// const subs = string.substr(4, 7);
+const subs = string.substr(4, 7);
 
 // substring
 // Returns the substring at the specified location within a String object.
-// const subs = string.substring(11, 15);
+const subs = string.substring(11, 15);
 
 // concat 
-// const a = ' dhaka';
-// const b = 'city'
-// const full = a + b;
-// const sub = full.concat();
-// const sub = full.concat().concat('abc').concat('  ').concat(123);
+const a = ' dhaka';
+const b = 'city'
+const full = a + b;
+const sub = full.concat();
+const sub = full.concat().concat('abc').concat('  ').concat(123);
 
 
-console.log(sub);
+// join()
+
+const words = ['a', 'b', 'c']
+const join = words.join(' ')
+const join = words.join('alim', 'badon');
+const join = words.join(1);
+console.log(join);
