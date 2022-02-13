76fadedf015239cb3815a829398119ae4a8d04db
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-02-13 22:42:42 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const string = 'Amar Sonar BAngla Ami Tomai VAlobashi '
+const string2 = 'AMAR SONAR BANGLA AMI TOMAI VALOBASHI '
+if (string.toUpperCase() == string2.toUpperCase()) {
+    console.log(' This Two line is Correct ');
+} else {
+    console.log('nah line gulo thik nai ');
+}
